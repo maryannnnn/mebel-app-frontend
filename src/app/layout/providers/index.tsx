5a41a38a1e2aea266a94f4090d1d053aaaedec7c
Marian Polyak <info@neo-lines.com>
2025-06-18 21:41:30 +0300

    Up12

--- src/app/layout/providers/index.tsx
+++ src/app/layout/providers/index.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React from "react";
-import { ApolloWrapper } from './ApolloWrapper'
+import { ApolloWrapper } from './ApolloWrapper.tsx'
 // import { ThemeProvider } from './ThemeProvider' // если нужно
 
 export function Providers({ children }: { children: React.ReactNode }) {
